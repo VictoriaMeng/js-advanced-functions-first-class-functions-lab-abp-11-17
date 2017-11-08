df215007cbdc78558925d7917370474e7a14b25b
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 21:49:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
-const myFunc = returnFirstTwoDrivers(drivers);
+
 
 function returnFirstTwoDrivers(drivers) {
   return drivers.slice(0, 2);
 }
+
+const myFunc = returnFirstTwoDrivers(drivers);
